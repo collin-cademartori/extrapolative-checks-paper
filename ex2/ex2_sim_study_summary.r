515c474f2b2a6f2a7116c3e6df4341afacc4b89c
collin-cademartori <cademac@wfu.edu>
2026-08-10 13:26:39 -0400

    More breaks ex2 overfit plot

--- ex2/ex2_sim_study_summary.r
+++ ex2/ex2_sim_study_summary.r
@@ -145,7 +145,7 @@ overfit_plot <- ggplot(data = sim_study_overfit) +
   facet_grid(vars(sim), vars(time), scales = "free") +
   xlab("Modeled Long-Run Correlation (Treated vs Spuriously Correlated Units)") +
   ylab("Average Standardized Error of Posterior\n Expected Treatment Effect") +
-  scale_x_continuous(expand = expansion(mult = 0.6), n.breaks = 2) +
+  scale_x_continuous(expand = expansion(mult = 0.6), n.breaks = 4) +
   scale_y_continuous(expand = expansion(mult = 0.1)) +
   theme_bw() +
   theme(panel.grid = element_blank()) +
